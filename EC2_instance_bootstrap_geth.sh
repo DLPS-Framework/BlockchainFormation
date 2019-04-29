033ebeb6e1ede30624de1f743a2288dbf5c55e2b
Philipp Ross <Philipp.P.Ross@bmwgroup.com>
2019-04-29 09:53:48 +0200

    Additions to Ethereum network setup

--- EC2_instance_bootstrap_geth.sh
+++ EC2_instance_bootstrap_geth.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-  echo "Hello from user-data!"
   # Settings -> enable tracing for commands
   set -x
 
@@ -81,7 +80,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
   #https://github.com/bas-vk/config/blob/master/geth-systemd-service.md
   #https://gist.github.com/xiaoping378/4eabb1915ec2b64a06e5b7d996bb8214
   IP=$( hostname -I )
-  bash -c  "sudo printf '%s\n' '[Unit]' 'Description=Ethereum go client' '[Service]' 'Type=simple' 'ExecStart=/usr/bin/geth --datadir /data/gethNetwork/node/ --networkid 31 --verbosity 3 --port 30310 --rpc --rpcaddr 0.0.0.0  --rpcapi db,clique,miner,eth,net,web3,personal,web3,admin --nat=extip:$IP  --unlock $FORMATTED_ACC_ID --password /data/gethNetwork/password.txt' 'StandardOutput=file:/var/log/geth.log' '[Install]' 'WantedBy=default.target' > /ect/systemd/system/geth.service"
+  bash -c  "sudo printf '%s\n' '[Unit]' 'Description=Ethereum go client' '[Service]' 'Type=simple' 'ExecStart=/usr/bin/geth --datadir /data/gethNetwork/node/ --networkid 31 --verbosity 3 --port 30310 --rpc --rpcaddr 0.0.0.0  --rpcapi db,clique,miner,eth,net,web3,personal,web3,admin --nat=extip:$IP  --unlock $FORMATTED_ACC_ID --password /data/gethNetwork/password.txt' 'StandardOutput=file:/var/log/geth.log' '[Install]' 'WantedBy=default.target' > /etc/systemd/system/geth.service"
   #systemctl --user enable geth.service
   
   #add log rotate
