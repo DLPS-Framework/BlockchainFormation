b5135f5536d9d380569b7c35bf4f4a80619be0dd
Philipp Ross (FG-26) <Philipp.P.Ross@bmw.de>
2019-07-30 14:10:02 +0200

    Adjust mounting mechanism to handle multiple device names (temp FIX, not nice)

--- BlockchainFormation/UserDataScripts/EC2_instance_bootstrap_base_no_proxy.sh
+++ BlockchainFormation/UserDataScripts/EC2_instance_bootstrap_base_no_proxy.sh
@@ -25,13 +25,25 @@ exec > >(tee /var/log/user_data.log|logger -t user-data -s 2>/dev/console) 2>&1
   APT::Periodic::Unattended-Upgrade "1";" >> /etc/apt/apt.conf.d/20auto-upgrades
 
 
+  UNMOUNTED=`lsblk --noheadings --raw -o NAME,MOUNTPOINT | awk '$1~/[[:digit:]]/ && $2 == ""'`
+  echo $UNMOUNTED
   #mounting disk
   # On some instance types the disk has different name
   mkdir /data
-  mkfs -t ext4 /dev/xvdb || mkfs -t ext4 /dev/nvme0n1
-  mount /dev/xvdb /data || mount /dev/nvme0n1 /data
+  #mkfs -t ext4 /dev/xvdb || mkfs -t ext4 /dev/nvme0n1
+  mkfs -t ext4 /dev/xvdb || yes N | mkfs -t ext4 /dev/nvme0n1 || yes N | mkfs -t ext4 /dev/nvme1n1
+  mount /dev/xvdb /data || mount /dev/nvme0n1 /data || mount /dev/nvme1n1 /data
+
+  #TEMP FIX
   DISKUUID=`sudo file -s /dev/xvdb | awk '{print $8}'`
   bash -c  "echo '$DISKUUID       /data   ext4    defaults,nofail        0       2' >> /etc/fstab"
+
+  DISKUUID=`sudo file -s /dev/nvme0n1 | awk '{print $8}'`
+  bash -c  "echo '$DISKUUID       /data   ext4    defaults,nofail        0       2' >> /etc/fstab"
+
+  DISKUUID=`sudo file -s /dev/nvme1n1 | awk '{print $8}'`
+  bash -c  "echo '$DISKUUID       /data   ext4    defaults,nofail        0       2' >> /etc/fstab"
+
   bash -c  "sudo chown -R ubuntu:ubuntu /data"
 
   # switch to normal user
