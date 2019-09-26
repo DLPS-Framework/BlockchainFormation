549ccf295aaf43eef443bf26d5d619d0cb93ad3f
Philipp Ross (FG-26) <Philipp.P.Ross@bmw.de>
2019-09-26 10:50:41 +0200

    Added NOTICE file

--- BlockchainFormation/blockchain_specifics/Fabric/chaincode/benchcontract/benchcontract.js
+++ BlockchainFormation/blockchain_specifics/Fabric/chaincode/benchcontract/benchcontract.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 BMW Group
+ * Copyright 2019  Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
