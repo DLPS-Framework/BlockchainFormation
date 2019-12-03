def8be6423a3298c24fc1d62822dfd6b7ac0f71b
Johannes Sedlmeir <johannes.sedlmeir@partner.bmwgroup.com>
2019-12-03 13:04:21 +0100

    Added indy-specific client with minimal installation requirements

---
+++ BlockchainFormation/UserDataScripts/EC2_instance_bootstrap_indy_client.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+  # Getting updates (and upgrades)
+  sudo apt-get update
+  sudo apt-get -y upgrade || echo "Upgrading in indy_bootstrap failed" >> /home/ubuntu/upgrade_fail2.log
+
+  sudo add-apt-repository ppa:deadsnakes/ppa -y
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68DB5E88 || echo "Adding keyserver failed" >> /home/ubuntu/upgrade_fail2.log
+	sudo apt-get update
+
+	sudo apt-get install -y python3.5 python3-pip python3.5-dev
+	sudo apt-get install -y libindy libindy-crypto=0.4.5
+	sudo pip3 install python3-indy==1.11.0
+
+  sudo mkdir /etc/indy /var/log/indy /var/lib/indy /var/lib/indy/backup /var/lib/indy/plugins
+  sudo chown -R ubuntu:ubuntu /var/log/indy/ /var/lib/indy/ /etc/indy/
+
+  # =======  Create success indicator at end of this script ==========
+  sudo touch /var/log/user_data_success.log
+
+EOF
