09fc23407e6db2926d0b45f239f009fd8a9f157f
JSedlmeir92@partner@bmwgroup.com <johannes.sedlmeir@fim-rc.de>
2020-01-02 11:08:53 +0100

    Enlarging docker swarm overlay network to account for issues with large fabric system startup

--- BlockchainFormation/blockchain_specifics/Fabric/Fabric.py
+++ BlockchainFormation/blockchain_specifics/Fabric/Fabric.py
@@ -135,7 +135,8 @@ def fabric_startup(config, logger, ssh_clients, scp_clients):
 
     # Name of the swarm network
     my_net = "my-net"
-    stdin, stdout, stderr = ssh_clients[0].exec_command(f"sudo docker network create --attachable --driver overlay {my_net}")
+    # stdin, stdout, stderr = ssh_clients[0].exec_command(f"sudo docker network create --attachable --driver overlay {my_net}")
+    stdin, stdout, stderr = ssh_clients[0].exec_command(f"sudo docker network create --subnet 10.10.0.0/16 --attachable --driver overlay {my_net}")
     out = stdout.readlines()
     # logger.debug(out)
     # logger.debug("".join(stderr.readlines()))
