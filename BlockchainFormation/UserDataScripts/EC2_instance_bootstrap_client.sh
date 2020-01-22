a9a3861d550d461e75b5a7fdecfb62ed302d05da
JSedlmeir92@partner@bmwgroup.com <johannes.sedlmeir@fim-rc.de>
2020-01-22 19:20:57 +0100

    Adding docker installation to general client startup

--- BlockchainFormation/UserDataScripts/EC2_instance_bootstrap_client.sh
+++ BlockchainFormation/UserDataScripts/EC2_instance_bootstrap_client.sh
@@ -25,6 +25,35 @@
   echo "nvm version: $(nvm version)"
   sudo apt update
 
+   # Getting updates (and upgrades)
+  sudo apt-get update
+  sudo apt-get -y upgrade || echo "Upgrading in indy_bootstrap failed" >> /home/ubuntu/upgrade_fail2.log
+
+   # Getting curl
+  sudo apt install curl
+
+  # Installing docker
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  # Testing the installation
+  docker --version
+  sudo docker run hello-world
+
+  # Installing docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  # Testing the installation
+  docker-compose --version
+
+  # Eventually user permissions need to be adjusted... rebooting required!
+  sudo usermod -aG docker ubuntu
+  newgrp docker
+  # Testing whether docker runs without user permissions
+  docker run hello-world
 
   # =======  Create success indicator at end of this script ==========
   sudo touch /var/log/user_data_success.log
