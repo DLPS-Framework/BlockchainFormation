d09bdc2195ba81562961bec3394f96e659ce7453
JSedlmeir92@partner@bmwgroup.com <johannes.sedlmeir@fim-rc.de>
2020-02-05 22:04:07 +0100

    Small changes due to restructuring of config file

--- BlockchainFormation/vm_handler.py
+++ BlockchainFormation/vm_handler.py
@@ -123,9 +123,9 @@ class VMHandler:
         elif self.config['blockchain_type'] == 'fabric':
 
             os.system(f"cp {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric.sh {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh")
-            os.system(f"sed -i -e 's/substitute_fabric_version/{self.config['fabric_settings']['version']['fabric_version']}/g' {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh")
-            os.system(f"sed -i -e 's/substitute_fabric_ca_version/{self.config['fabric_settings']['version']['fabric_ca_version']}/g' {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh")
-            os.system(f"sed -i -e 's/substitute_fabric_thirdparty_version/{self.config['fabric_settings']['version']['thirdparty_version']}/g' {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh")
+            os.system(f"sed -i -e 's/substitute_fabric_version/{self.config['fabric_settings']['fabric_version']}/g' {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh")
+            os.system(f"sed -i -e 's/substitute_fabric_ca_version/{self.config['fabric_settings']['fabric_ca_version']}/g' {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh")
+            os.system(f"sed -i -e 's/substitute_fabric_thirdparty_version/{self.config['fabric_settings']['thirdparty_version']}/g' {dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh")
 
             with open(f"{dir_name}/UserDataScripts/EC2_instance_bootstrap_fabric_temp.sh", 'r') as content_file:
                 user_data_specific = content_file.read()
