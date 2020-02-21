a01d8f8f6d2d81e548a1acc3e42cb9d2b30bfce3
JSedlmeir92@partner@bmwgroup.com <johannes.sedlmeir@fim-rc.de>
2020-02-22 00:35:41 +0100

    Further developing - first cross-node transaction now running in shell

--- BlockchainFormation/UserDataScripts/EC2_instance_bootstrap_corda.sh
+++ BlockchainFormation/UserDataScripts/EC2_instance_bootstrap_corda.sh
@@ -4,6 +4,31 @@
   sudo apt-get update
   sudo apt-get -y upgrade || echo "Upgrading in indy_bootstrap failed" >> /home/ubuntu/upgrade_fail2.log
 
+ # Getting curl
+  sudo apt install curl
+
+  # Installing docker
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  # Testing the installation
+  docker --version
+  sudo docker run hello-world
+
+  # Installing docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  # Testing the installation
+  docker-compose --version
+
+  # Eventually user permissions need to be adjusted... rebooting required!
+  sudo usermod -aG docker ubuntu
+  newgrp docker
+  # Testing whether docker runs without user permissions
+  docker run hello-world
   sudo apt install git
   git --version
 
@@ -16,10 +41,13 @@
   java -version
 
   (cd /data && git clone https://github.com/corda/samples)
-  (cd /data/samples/cordapp-example && ./gradlew deployNodes)
 
+  cp -r /data/samples /home/ubuntu
+
+  # curl https://software.r3.com/artifactory/corda-releases/net/corda/corda-tools-network-bootstrapper/4.3-RC04/corda-tools-network-bootstrapper-4.3-RC04.jar --output /data/samples/cordapp-example/bootstrapper.jar
 
   # =======  Create success indicator at end of this script ==========
   sudo touch /var/log/user_data_success.log
+  sudo reboot
 
 EOF
--- BlockchainFormation/blockchain_specifics/Corda/Corda.py
+++ BlockchainFormation/blockchain_specifics/Corda/Corda.py
@@ -37,6 +37,55 @@ def corda_startup(config, logger, ssh_clients, scp_clients):
     :return:
     """
 
+    # Creating docker swarm
+    logger.info("Preparing & starting docker swarm")
+
+    stdin, stdout, stderr = ssh_clients[0].exec_command("sudo docker swarm init")
+    out = stdout.readlines()
+    # for index, _ in enumerate(out):
+    #     logger.debug(out[index].replace("\n", ""))
+
+    # logger.debug("".join(stderr.readlines()))
+
+    stdin, stdout, stderr = ssh_clients[0].exec_command("sudo docker swarm join-token manager")
+    out = stdout.readlines()
+    # logger.debug(out)
+    # logger.debug("".join(stderr.readlines()))
+    join_command = out[2].replace("    ", "").replace("\n", "")
+
+    for index, _ in enumerate(config['priv_ips']):
+
+        if index != 0:
+            stdin, stdout, stderr = ssh_clients[index].exec_command("sudo " + join_command)
+            out = stdout.readlines()
+            logger.debug(out)
+            logger.debug("".join(stderr.readlines()))
+
+    # Name of the swarm network
+    my_net = "my-net"
+    stdin, stdout, stderr = ssh_clients[0].exec_command(f"sudo docker network create --subnet 10.10.0.0/16 --attachable --driver overlay {my_net}")
+    out = stdout.readlines()
+    logger.debug(out)
+    logger.debug("".join(stderr.readlines()))
+    network = out[0].replace("\n", "")
+
+    time.sleep(5)
+
+    logger.info("Testing whether setup was successful")
+    stdin, stdout, stderr = ssh_clients[0].exec_command("sudo docker node ls")
+    out = stdout.readlines()
+    for index, _ in enumerate(out):
+        logger.debug(out[index].replace("\n", ""))
+
+    logger.debug("".join(stderr.readlines()))
+    if len(out) == len(config['priv_ips']) + 1:
+        logger.info("Docker swarm started successfully")
+    else:
+        logger.info("Docker swarm setup was not successful")
+        logger.info(f"Expected length: {len(config['priv_ips']) + 1}, actual length: {len(out)}")
+        # sys.exit("Fatal error when performing docker swarm setup")
+
+
     logger.info(f"Started Corda")
 
     logger.info("Uploading the customized build.gradle and generating node documents")
@@ -54,27 +103,28 @@ def corda_startup(config, logger, ssh_clients, scp_clients):
 
     logger.info("Getting the generated files and distributing them to the nodes")
     scp_clients[0].get("/data/samples/cordapp-example/workflows-kotlin/build/nodes", f"{config['exp_dir']}/setup", recursive=True)
-    stdin, stdout, stderr = ssh_clients[0].exec_command("(cd /data/samples/cordapp-example/workflows-kotlin/build && rm -r nodes && mkdir nodes)")
+    stdin, stdout, stderr = ssh_clients[0].exec_command("(cd /data/samples/cordapp-example/workflows-kotlin && rm -r build)")
     logger.debug(stdout.readlines())
     logger.debug(stderr.readlines())
     for node, _ in enumerate(config['priv_ips']):
         stdin, stdout, stderr = ssh_clients[node].exec_command("(cd /data/samples/cordapp-example/workflows-kotlin && mkdir build && cd build && mkdir nodes)")
         logger.debug(stdout.readlines())
         logger.debug(stderr.readlines())
-        scp_clients[node].put(f"{config['exp_dir']}/setup/nodes/Party{node}", "/data/samples/cordapp-example/workflows-kotlin/build/nodes", recursive=True)
-        scp_clients[node].put(f"{config['exp_dir']}/setup/nodes/Party{node}_node.conf", "/data/samples/cordapp-example/workflows-kotlin/build/nodes")
-        
+
+        if node == 0:
+            scp_clients[node].put(f"{config['exp_dir']}/setup/nodes/Notary", "/data/samples/cordapp-example/workflows-kotlin/build/nodes", recursive=True)
+            scp_clients[node].put(f"{config['exp_dir']}/setup/nodes/Notary_node.conf", "/data/samples/cordapp-example/workflows-kotlin/build/nodes")
+        else:
+            scp_clients[node].put(f"{config['exp_dir']}/setup/nodes/Party{node}", "/data/samples/cordapp-example/workflows-kotlin/build/nodes", recursive=True)
+            scp_clients[node].put(f"{config['exp_dir']}/setup/nodes/Party{node}_node.conf", "/data/samples/cordapp-example/workflows-kotlin/build/nodes")
+
     logger.info("Starting all nodes")
     channels = []
     for node, _ in enumerate(config['priv_ips']):
-        channel = ssh_clients[node].get_transport().open_session()
-        channel.exec_command(f"(cd /data/samples/cordapp-example && ./gradlew runParty{node}Server >> ~/node.log)")
-        channels.append(channel)
-
-    # Checking whether the system has connected
-
-    # transaction with node terminal
-    # stdin, stdout, stderr = ssh_clients[0].exec_command('(cd /data/corda/cordapp-example/ && flow start ExampleFlow$Initiator iouValue: 50, otherParty: "O=PartyB,L=New York,C=US")')
+        if node != 0:
+            channel = ssh_clients[node].get_transport().open_session()
+            channel.exec_command(f"(cd /data/samples/cordapp-example/workflows-kotlin/build/nodes/Party{node} && java -jar corda.jar >> ~/node.log 2>&1)")
+            channels.append(channel)
 
 
 
@@ -100,31 +150,38 @@ def write_config(config, logger):
     f.write("    }\n")
 
     for node, ip in enumerate(config['priv_ips']):
-        """
-        f.write("    node {\n")
-        f.write(f"        name 'O=Notary{node},L=London,C=GB'\n")
-        f.write("        notary = [validating : false]\n")
-        f.write("        p2pPort 10000\n")
-        f.write("        rpcSettings {\n")
-        f.write(f"            address('{ip}:10001')\n")
-        f.write(f"            adminAddress('{ip}:10002')\n")
-        f.write("        }\n")
-        f.write("        projectCordapp {\n")
-        f.write("            deploy = false\n")
-        f.write("        }\n")
-        f.write("        cordapps.clear()\n")
-        f.write("    }\n")
-        """
 
-        f.write("    node {\n")
-        f.write(f"        name 'O=Party{node},L=London,C=GB'\n")
-        f.write("        p2pPort 10000\n")
-        f.write("        rpcSettings {\n")
-        f.write(f"            address('{ip}:10001')\n")
-        f.write(f"            adminAddress('{ip}:10002')\n")
-        f.write("        }\n")
-        f.write("        rpcUsers = [[user: 'user1', 'password': 'test', 'permissions': ['ALL']]]\n")
-        f.write("    }\n")
+        if node == 0:
+
+            f.write("    node {\n")
+            f.write(f"        name 'O=Notary,L=London,C=GB'\n")
+            f.write(f"        notary = [validating : false]\n")
+            f.write(f"        p2pAddress ('{ip}:10000')\n")
+            f.write("        rpcSettings {\n")
+            f.write(f"            address('{ip}:10001')\n")
+            f.write(f"            adminAddress('localhost:10002')\n")
+            f.write("        }\n")
+            f.write("        projectCordapp {\n")
+            f.write(f"            deploy = false\n")
+            f.write("        }\n")
+            f.write(f"        cordapps.clear()\n")
+            f.write("    }\n")
+
+        else:
+
+            f.write("    node {\n")
+            f.write(f"        name 'O=Party{node},L=London,C=GB'\n")
+            f.write(f"        p2pAddress ('{ip}:10000')\n")
+            f.write("        rpcSettings {\n")
+            f.write(f"            address('{ip}:10001')\n")
+            f.write(f"            adminAddress('localhost:10002')\n")
+            f.write("        }\n")
+            # f.write("        artemisPort 10002\n")
+            # f.write("        webAddress ('0.0.0.0:10003')\n")
+            # f.write("        webPort 10003")
+            # f.write("        devMode = true")
+            f.write(f"        rpcUsers = [[user: 'user1', 'password': 'test', 'permissions': ['ALL']]]\n")
+            f.write("    }\n")
 
     f.write("}\n")
 
