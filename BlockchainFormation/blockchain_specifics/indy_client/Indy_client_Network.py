6744b73c032c68b38130fa1b6a02d54b657f1d8c
JSedlmeir92@partner@bmwgroup.com <johannes.sedlmeir@fim-rc.de>
2020-04-06 03:32:49 +0200

    Modifying the indy client setup

---
+++ BlockchainFormation/blockchain_specifics/indy_client/Indy_client_Network.py
@@ -0,0 +1,87 @@
+#  Copyright 2020 ChainLab
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+import json
+
+from BlockchainFormation.utils.utils import *
+
+
+class Indy_client_Network:
+
+    @staticmethod
+    def shutdown(node_handler):
+        pass
+
+    @staticmethod
+    def startup(node_handler):
+        """
+        Startup for the blockchain client option
+        :param config:
+        :param logger:
+        :param ssh_clients:
+        :param scp_clients:
+        :return:
+        """
+
+        logger = node_handler.logger
+        config = node_handler.config
+        ssh_clients = node_handler.ssh_clients
+        scp_clients = node_handler.scp_clients
+
+        if config["client_settings"]["target_network_conf"] is not None:
+            # Attach client IPs to network conf
+            Indy_client_Network.attach_to_blockchain_conf(node_handler)
+
+    @staticmethod
+    def attach_to_blockchain_conf(node_handler):
+        """
+        Attach client settings to another config
+        :param config:
+        :param logger:
+        :return:
+        """
+
+        config = node_handler.config
+        logger = node_handler.logger
+
+        try:
+            with open(config["client_settings"]["target_network_conf"]) as json_file:
+                network_config_file = json.load(json_file)
+        except:
+            logger.error("ERROR: Problem loading the given config file")
+
+        network_config_file["client settings"] = {
+
+            "ips": config["ips"],
+            "exp_dir": config["exp_dir"]
+
+        }
+
+        try:
+            network_config_file['launch_times'] = config['launch_times'],
+            network_config_file['vpc_ids'] = config['vpc_ids']
+            network_config_file['instance_ids'] = config['instance_ids']
+
+        except Exception as e:
+            logger.info("No vpc_ids and instance_ids available")
+
+        logger.info("Attaching client config to parent network config now")
+        logger.info(f"Target parent network: {config['client_settings']['target_network_conf']}")
+        if config['public_ip']:
+            network_config_file["client settings"]['public_ips'] = 'public_ips'
+
+        # write network config back
+        with open(f"{config['client_settings']['target_network_conf']}", 'w') as outfile:
+            json.dump(network_config_file, outfile, default=datetimeconverter, indent=4)
